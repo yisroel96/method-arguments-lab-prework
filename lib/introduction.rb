9e94a33ee9d822abbaf314693aeb17eb3086c297
witty-collection-3981 <yisroel96@gmail.com>
2018-11-21 16:38:23 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,4 +3,4 @@ def introduction(name)
 end
 
 :introduction_with_language(name, language)
-puts "Hi, my name is #{name} and I am learning to program in #{language}."
+puts "Hi, my name is #{"Dan"} and I am learning to program in #{language}."
